4c8764e3458c78b282db92fb7e44f73ea9911ed7
笹沼 透 <sasanuma@kanagawa-jk.or.jp>
2017-01-20 12:36:03 +0900

    テストプロジェクトの追加。

---
+++ SampleApplication/PrimitiveObject/名称.vb
@@ -0,0 +1,17 @@
+﻿Namespace PrimitiveObject
+    Public Class 名称
+
+        Private m_値 As String = String.Empty
+
+        Public Sub New(値 As String)
+            m_値 = 値
+        End Sub
+
+        Public ReadOnly Property 値 As String
+            Get
+                Return m_値
+            End Get
+        End Property
+
+    End Class
+End Namespace
---
+++ UnitTest/名称クラステスト.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class 名称クラステスト
+
+    <TestMethod()> Public Sub 名称クラスのテスト()
+
+        Dim foo As New SampleApplication.PrimitiveObject.名称("日本")
+        Assert.AreEqual("日本", foo.値)
+
+    End Sub
+
+End Class
