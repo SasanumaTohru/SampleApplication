a428746f1c63b66cb4f9033a715fa30527ff1c0f
笹沼 透 <sasanuma@kanagawa-jk.or.jp>
2017-01-20 13:01:31 +0900

    サンプルソリューションとしての基本構成要素を整えました。

--- SampleApplication/PrimitiveObject/名称.vb
+++ SampleApplication/PrimitiveObject/名称.vb
@@ -3,10 +3,18 @@
 
         Private m_値 As String = String.Empty
 
+        ''' <summary>
+        ''' コンストラクタ
+        ''' </summary>
+        ''' <param name="値">名称となる文字列を設定します。</param>
         Public Sub New(値 As String)
-            m_値 = 値
+            m_値 = Trim(値)
         End Sub
 
+        ''' <summary>
+        ''' 値プロパティ
+        ''' </summary>
+        ''' <returns>名称を返します。</returns>
         Public ReadOnly Property 値 As String
             Get
                 Return m_値
--- UnitTest/名称クラステスト.vb
+++ UnitTest/名称クラステスト.vb
@@ -5,8 +5,21 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
     <TestMethod()> Public Sub 名称クラスのテスト()
 
-        Dim foo As New SampleApplication.PrimitiveObject.名称("日本")
-        Assert.AreEqual("日本", foo.値)
+        Const 入力値 As String = "日本"
+
+        Const 不正値1 As String = "　日本　"
+        Const 不正値2 As String = " 日本 "
+
+        '名称クラスを生成する時は、名称となる文字列を設定する。
+        Dim 国名 As New SampleApplication.PrimitiveObject.名称(入力値)
+        '値プロパティは、名称となる文字列を返す。
+        Assert.AreEqual(入力値, 国名.値)
+
+        'インスタンス生成時に値の先頭や末尾にスペースが含まれている場合、コンストラクタで除去する。
+        Dim 国名2 As New SampleApplication.PrimitiveObject.名称(不正値1)
+        Assert.AreEqual(入力値, 国名2.値)
+        Dim 国名3 As New SampleApplication.PrimitiveObject.名称(不正値2)
+        Assert.AreEqual(入力値, 国名3.値)
 
     End Sub
 
