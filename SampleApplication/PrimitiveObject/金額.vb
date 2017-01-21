0040dbb1c0cb525d2b5829da4d69751370a1fd71
Satohru <satohru@ktd.biglobe.ne.jp>
2017-01-21 10:42:47 +0900

    金額クラスを追加。

---
+++ SampleApplication/PrimitiveObject/金額.vb
@@ -0,0 +1,22 @@
+﻿Namespace PrimitiveObject
+    Public Class 金額
+        Private m_値 As Decimal = 0
+
+        Public Sub New(値 As Decimal)
+            m_値 = 値
+        End Sub
+
+        Public ReadOnly Property 値 As Decimal
+            Get
+                Return m_値
+            End Get
+        End Property
+
+        Public ReadOnly Property 桁区切値 As String
+            Get
+                Return m_値.ToString("#,##0")
+            End Get
+        End Property
+
+    End Class
+End Namespace
---
+++ UnitTest/PrimitiveObjectのテスト/金額クラステスト.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class 金額クラステスト
+
+    Private Const テスト値1 As Decimal = 123456789
+
+    <TestMethod()> Public Sub 金額クラス基本テスト()
+
+        Dim 金額 As New SampleApplication.PrimitiveObject.金額(テスト値1)
+
+        Assert.AreEqual(テスト値1, 金額.値)
+
+        Assert.AreEqual("123,456,789", 金額.桁区切値)
+
+        '振る舞いの検討
+        'Assert.AreEqual("123,456,789円", 金額.値)
+        'Assert.AreEqual("\123,456,789", 金額.値)
+
+    End Sub
+
+End Class
