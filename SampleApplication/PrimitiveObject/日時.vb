c15ae34385ee785664f25825e25b2ba0e090730d
Satohru <satohru@ktd.biglobe.ne.jp>
2017-01-24 20:32:39 +0900

    日時クラスの拡張

--- SampleApplication/PrimitiveObject/日時.vb
+++ SampleApplication/PrimitiveObject/日時.vb
@@ -1,11 +1,32 @@
 ﻿Namespace PrimitiveObject
     Public Class 日時
 
-        Private m_値 As DateTime = DateTime.MinValue '0001/01/01 0:00:00
+        '値フィールド　初期値は「0001/01/01 0:00:00」
+        Private m_値 As Date = Date.MinValue
 
-        Public ReadOnly Property 値 As DateTime
+        ''' <summary>
+        ''' コンストラクタ
+        ''' </summary>
+        ''' <param name="値"></param>
+        Public Sub New(値 As Date)
+            m_値 = 値
+        End Sub
+
+        ''' <summary>
+        ''' 値プロパティ
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property 値 As Date
             Get
                 Return m_値
+            End Get
+        End Property
+
+        Public ReadOnly Property 和暦日付文字列 As String
+            Get
+                Dim 文化圏 As New System.Globalization.CultureInfo("ja-JP", False)
+                文化圏.DateTimeFormat.Calendar = New System.Globalization.JapaneseCalendar()
+                Return m_値.ToString("gy年M月d日", 文化圏)
             End Get
         End Property
 
--- UnitTest/PrimitiveObjectのテスト/日時クラスのテスト.vb
+++ UnitTest/PrimitiveObjectのテスト/日時クラスのテスト.vb
@@ -5,9 +5,21 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
     <TestMethod()> Public Sub 日時クラス基本テスト()
 
-        Dim 日付と時間 As New SampleApplication.PrimitiveObject.日時
-        '日時クラスの初期値は「0001/01/01 0:00:00」とする。
-        Assert.AreEqual("0001/01/01 0:00:00", 日付と時間.値.ToString)
+        'インスタンスの生成時に日付をセットする。
+        Dim 日付と時間 As New SampleApplication.PrimitiveObject.日時(#5/25/2017#)
+        Assert.AreEqual(#05/25/2017 00:00:00#, 日付と時間.値)
+
+        'インスタンスの生成時に日時をセットする。
+        Dim 日付と時間2 As New SampleApplication.PrimitiveObject.日時(#5/25/2017 08:45:00  PM#)
+        Assert.AreEqual(#05/25/2017 08:45:00 PM#, 日付と時間2.値)
+
+        '振る舞いの検討
+        'DateTime型のデフォルト機能
+        Assert.AreEqual("2017/05/25", 日付と時間.値.ToShortDateString)
+        Assert.AreEqual("20:45", 日付と時間2.値.ToShortTimeString)
+        'オリジナルの実装
+        Assert.AreEqual("平成29年5月25日", 日付と時間.和暦日付文字列)
+
 
     End Sub
 
