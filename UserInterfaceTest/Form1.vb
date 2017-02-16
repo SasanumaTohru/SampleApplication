daf0eac20385755c6e4b0a806579b05e97491627
Satohru <satohru@ktd.biglobe.ne.jp>
2017-02-16 22:06:15 +0900

    テスト用のUIプロジェクトを追加

---
+++ UserInterfaceTest/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim 商品ID As New SampleApplication.BusinessObject.商品.商品ID("999999")
+        Debug.Print(商品ID.値)
+    End Sub
+End Class
