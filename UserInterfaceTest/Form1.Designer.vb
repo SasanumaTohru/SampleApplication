a79486772ac1006178c73901b149170ea59d95b2
Satohru <satohru@ktd.biglobe.ne.jp>
2017-02-16 22:50:08 +0900

    UIに例外処理の基本パターンをコーディング

--- UserInterfaceTest/Form1.Designer.vb
+++ UserInterfaceTest/Form1.Designer.vb
@@ -22,29 +22,43 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmd実行 = New System.Windows.Forms.Button()
+        Me.txt商品ID = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'cmd実行
         '
-        Me.Button1.Location = New System.Drawing.Point(511, 280)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(297, 141)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.cmd実行.Location = New System.Drawing.Point(511, 280)
+        Me.cmd実行.Name = "cmd実行"
+        Me.cmd実行.Size = New System.Drawing.Size(297, 141)
+        Me.cmd実行.TabIndex = 0
+        Me.cmd実行.Text = "実行(&R)"
+        Me.cmd実行.UseVisualStyleBackColor = True
+        '
+        'txt商品ID
+        '
+        Me.txt商品ID.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.txt商品ID.Location = New System.Drawing.Point(227, 102)
+        Me.txt商品ID.Name = "txt商品ID"
+        Me.txt商品ID.Size = New System.Drawing.Size(340, 43)
+        Me.txt商品ID.TabIndex = 1
         '
         'Form1
         '
+        Me.AcceptButton = Me.cmd実行
         Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1096, 575)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt商品ID)
+        Me.Controls.Add(Me.cmd実行)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "テスト画面"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents cmd実行 As Button
+    Friend WithEvents txt商品ID As TextBox
 End Class
--- UserInterfaceTest/Form1.vb
+++ UserInterfaceTest/Form1.vb
@@ -1,6 +1,28 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim 商品ID As New SampleApplication.BusinessObject.商品.商品ID("999999")
-        Debug.Print(商品ID.値)
+﻿Imports SampleApplication
+
+Public Class Form1
+
+    ''' <summary>
+    ''' MessageBoxCaption構造体　※検証終了後にクラス化
+    ''' </summary>
+    Private Structure MessageBoxCaption
+        Public Const 入力エラー As String = "入力エラー"
+    End Structure
+
+    ''' <summary>
+    ''' 実行ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub cmd実行_Click(sender As Object, e As EventArgs) Handles cmd実行.Click
+        Try
+            Dim 商品ID As New BusinessObject.商品.商品ID(txt商品ID.Text)
+            Debug.Print(商品ID.値)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, MessageBoxCaption.入力エラー, MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            txt商品ID.Text = String.Empty
+        End Try
     End Sub
+
 End Class
