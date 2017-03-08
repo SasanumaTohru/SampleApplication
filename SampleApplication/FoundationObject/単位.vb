cc7838289e1ccfb41084b364e91e30ce07a8862b
Satohru <satohru@ktd.biglobe.ne.jp>
2017-03-08 21:11:47 +0900

    単位クラスの追加

---
+++ SampleApplication/FoundationObject/単位.vb
@@ -0,0 +1,50 @@
+﻿Namespace FoundationObject
+    Public Class 単位
+
+        Public Enum 単位リスト As Integer
+            個 = 1
+            枚 = 2
+            本 = 3
+            台 = 4
+            式 = 5
+        End Enum
+
+        Private m_値 As 単位リスト
+
+        ''' <summary>
+        ''' コンストラクタ
+        ''' </summary>
+        ''' <param name="値"></param>
+        Public Sub New(値 As 単位リスト)
+            単位の設定は正しい(値)
+            m_値 = 値
+        End Sub
+
+        Private Sub 単位の設定は正しい(値 As 単位リスト)
+            If 単位リスト.個 > 値 Or 値 > 単位リスト.式 Then
+                Throw New Exception("単位の設定が正しくありません。")
+            End If
+        End Sub
+
+        ''' <summary>
+        ''' 値プロパティ
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property 値 As 単位リスト
+            Get
+                Return m_値
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' 名称プロパティ
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property 名称 As PrimitiveObject.名称
+            Get
+                Return New PrimitiveObject.名称(m_値.ToString)
+            End Get
+        End Property
+
+    End Class
+End Namespace
---
+++ UnitTest/FoundationObject/単位クラステスト.vb
@@ -0,0 +1,21 @@
+﻿Imports SampleApplication
+
+<TestClass()> Public Class 単位クラステスト
+
+    <TestMethod()> <TestCategory("基本テスト")> Public Sub 単位クラスの基本テスト()
+
+        Dim 単位 As New FoundationObject.単位(FoundationObject.単位.単位リスト.式)
+        Assert.AreEqual("式", 単位.名称.値)
+        Assert.AreEqual(FoundationObject.単位.単位リスト.式, 単位.値)
+
+    End Sub
+
+    <TestMethod()> <TestCategory("例外テスト")> <ExpectedException(GetType(Exception))> Public Sub 例外テスト1()
+        Dim 単位 As New FoundationObject.単位(0)
+    End Sub
+
+    <TestMethod()> <TestCategory("例外テスト")> <ExpectedException(GetType(Exception))> Public Sub 例外テスト2()
+        Dim 単位 As New FoundationObject.単位(6)
+    End Sub
+
+End Class
