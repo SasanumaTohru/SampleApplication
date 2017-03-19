44075c2257bb0887709355062d0321a10f982844
Satohru <satohru@ktd.biglobe.ne.jp>
2017-03-19 19:38:51 +0900

    商品の価格を設計変更する前

---
+++ SampleApplication/BusinessObject/商品/適用価格.vb
@@ -0,0 +1,69 @@
+﻿Namespace BusinessObject.商品
+    Public Class 適用価格
+
+        Private m_商品ID As 商品ID
+        Private m_区分 As 区分リスト
+        Private m_価格 As PrimitiveObject.金額
+        Private m_適用開始日 As PrimitiveObject.日付
+
+        Public Enum 区分リスト As Integer
+            仕入 = 1
+            販売 = 2
+        End Enum
+
+        Public Sub New(商品ID As 商品ID, 区分 As 区分リスト, 価格 As PrimitiveObject.金額, 適用開始日 As PrimitiveObject.日付)
+            区分は正しい(区分)
+            価格は1円以上である(価格)
+            適用開始日が明日以降である(適用開始日)
+
+            m_商品ID = 商品ID
+            m_区分 = 区分
+            m_価格 = 価格
+            m_適用開始日 = 適用開始日
+        End Sub
+
+        Private Sub 区分は正しい(区分 As 区分リスト)
+            If 区分 < 区分リスト.仕入 Or 区分 > 区分リスト.販売 Then
+                Throw New Exception("価格区分の設定が正しくありません。")
+            End If
+        End Sub
+
+        Private Sub 価格は1円以上である(価格 As PrimitiveObject.金額)
+            If 価格.値 < 1D Then
+                Throw New Exception("価格は1円以上の金額で設定します。")
+            End If
+        End Sub
+
+        Private Sub 適用開始日が明日以降である(適用開始日 As PrimitiveObject.日付)
+            Dim 設定できる日付 As New PrimitiveObject.日付(Date.Today.AddDays(1))
+            If 適用開始日.値 < 設定できる日付.値 Then
+                Throw New Exception("適用開始日には明日以降の日付が必要です。")
+            End If
+        End Sub
+
+        Public ReadOnly Property 商品ID As 商品ID
+            Get
+                Return m_商品ID
+            End Get
+        End Property
+
+        Public ReadOnly Property 価格 As PrimitiveObject.金額
+            Get
+                Return m_価格
+            End Get
+        End Property
+
+        Public ReadOnly Property 区分 As 区分リスト
+            Get
+                Return m_区分
+            End Get
+        End Property
+
+        Public ReadOnly Property 適用開始日 As PrimitiveObject.日付
+            Get
+                Return m_適用開始日
+            End Get
+        End Property
+
+    End Class
+End Namespace
---
+++ UnitTest/BusinessObject/価格クラステスト.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports SampleApplication.BusinessObject
+Imports SampleApplication.PrimitiveObject
+
+<TestClass()> Public Class 価格クラステスト
+
+    <TestMethod()> <TestCategory("基本テスト")> Public Sub 価格クラスの基本テスト()
+
+        Dim 価格を変更する商品 As New 商品.商品ID("999999")
+        Dim 価格変更日 As New 日付(Today.AddDays(1))
+
+        Dim 販売価格 As New 商品.適用価格(
+            価格を変更する商品,
+            商品.適用価格.区分リスト.販売,
+            New 金額(100),
+            価格変更日)
+
+        Assert.AreEqual("999999", 販売価格.商品ID.値)
+        Assert.AreEqual("販売", 販売価格.区分.ToString)
+        Assert.AreEqual(100D, 販売価格.価格.値)
+        Assert.AreEqual(#3/20/2017#, 販売価格.適用開始日.値)
+
+        Dim 仕入価格 As New 商品.適用価格(価格を変更する商品, 商品.適用価格.区分リスト.仕入, New 金額(90), 価格変更日)
+        Assert.AreEqual("仕入", 仕入価格.区分.ToString)
+
+    End Sub
+
+    <TestMethod()> <TestCategory("例外テスト")> <ExpectedException(GetType(Exception))> Public Sub 価格クラスの例外テスト1()
+        '価格区分下限オーバー
+        Dim 販売価格 As New 商品.適用価格(New 商品.商品ID("999999"), 0, New 金額(100), New 日付(Today.AddDays(1)))
+    End Sub
+
+    <TestMethod()> <TestCategory("例外テスト")> <ExpectedException(GetType(Exception))> Public Sub 価格クラスの例外テスト2()
+        '価格区分上限オーバー
+        Dim 販売価格 As New 商品.適用価格(New 商品.商品ID("999999"), 3, New 金額(100), New 日付(Today.AddDays(1)))
+    End Sub
+
+    <TestMethod()> <TestCategory("例外テスト")> <ExpectedException(GetType(Exception))> Public Sub 価格クラスの例外テスト3()
+        '適用開始日が明日以降になっていない
+        Dim 販売価格 As New 商品.適用価格(New 商品.商品ID("999999"), 商品.適用価格.区分リスト.販売, New 金額(100), New 日付(Today.AddDays(0)))
+    End Sub
+
+    <TestMethod()> <TestCategory("例外テスト")> <ExpectedException(GetType(Exception))> Public Sub 価格クラスの例外テスト4()
+        '金額が1円以上ではない
+        Dim 販売価格 As New 商品.適用価格(New 商品.商品ID("999999"), 商品.適用価格.区分リスト.販売, New 金額(0), New 日付(Today.AddDays(1)))
+    End Sub
+
+End Class
